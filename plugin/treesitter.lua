9698a359455eb180a77397612d74962f3b876eae
roufsyed <afrid704@gmail.com>
2021-09-28 15:42:51 +0530

    Updated treesitter to download only what is needed.

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = "all",
+    ensure_installed = { "c", "cpp", "java", "python", "kotlin", "html", "css", "javascript", "json", "bash", "graphql", "lua", "php", "typescript", "yaml", "vue", "vim", "dockerfile", "tsx", "r", "query" },
 
     highlight = {
         enable = true,
